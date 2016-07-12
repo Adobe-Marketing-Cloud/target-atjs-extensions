d77ea4d4d8390eaa13270f20f65d645a988c9c86
Andrei Anischevici <anischev@adobe.com>
2016-07-12 17:26:38 +0300

    Wrap extension in an IIFE Refactor 'options' factory

--- angular/directive/src/at-angular-directive.js
+++ angular/directive/src/at-angular-directive.js
@@ -15,86 +15,84 @@
 
 /* global adobe, angular */
 'use strict';
-
-function ServiceFunction(options, promise, log) {
-  var self = this;
-  promise = promise || {
-    defer: function () {
-      return {resolve: function () {}, promise: function () {}};
-    }}; // empty promise
-  return {
-    data: null, // temporarily store Target response
-    // promise resolver
-    getOffer: function () {
-      log('service.getOffer');
-      var defer = promise.defer();
-      // adobe.target API call to get a Target offer
-      var offer = {
-        mbox: options.mbox,
-        success: function (response) {
-          log('getOffer success', response);
-          self.data = response;
-          defer.resolve(response); // promise is resolved
-        },
-        error: function (status, error) {
-          log('getOffer error', error);
-          defer.resolve(); // promise is resolved to continue app execution
+(function () {
+  function ServiceFunction(options, promise, log) {
+    var self = this;
+    return {
+      data: null, // temporarily store Target response
+      // promise resolver
+      getOffer: function () {
+        log('service.getOffer');
+        var defer = promise.defer();
+        // adobe.target API call to get a Target offer
+        var offer = {
+          mbox: options.mbox,
+          success: function (response) {
+            log('getOffer success', response);
+            self.data = response;
+            defer.resolve(response); // promise is resolved
+          },
+          error: function (status, error) {
+            log('getOffer error', error);
+            defer.resolve(); // promise is resolved to continue app execution
+          }
+        };
+        // Add optional properties
+        if (options.params) {
+          offer.params = options.params;
         }
-      };
-      // Add optional properties
-      if (options.params) {
-        offer.params = options.params;
-      }
-      if (options.timeout) {
-        offer.timeout = options.timeout;
-      }
-      adobe.target.getOffer(offer); // Target call
-      return defer.promise;
-    },
-    applyOffer: function () {
-      log('service.applyOffer');
-      var data = self.data;
-      if (data && data.length > 0) {
-        var offer = {offer: data};
-        // add optional selector if defined
-        if (options.selector) {
-          offer.selector = options.selector;
+        if (options.timeout) {
+          offer.timeout = options.timeout;
         }
-        log('applyOffer', offer);
-        // adobe.target API call method to inject data to DOM
-        adobe.target.applyOffer(offer);
-        // clear data after use
-        self.data = null;
+        adobe.target.getOffer(offer); // Target call
+        return defer.promise;
+      },
+      applyOffer: function () {
+        log('service.applyOffer');
+        var data = self.data;
+        if (data && data.length > 0) {
+          var offer = {offer: data};
+          // add optional selector if defined
+          if (options.selector) {
+            offer.selector = options.selector;
+          }
+          log('applyOffer', offer);
+          // adobe.target API call method to inject data to DOM
+          adobe.target.applyOffer(offer);
+          // clear data after use
+          self.data = null;
+        }
       }
-    }
-  };
-}
-
-adobe.target.registerExtension({
-  name: 'angular-directive',
-  modules: ['settings', 'logger'],
-  register: function (settings, logger) {
-    angular.module('angular-directive.lib', [])
-      .constant('version', '0.3.0')
-      .constant('settings', settings)
-      .constant('logger', logger)
-      .constant('customOptions', {})
-
-      .factory('options', ['settings', 'customOptions',
-        function (settings, opts) {
-          return {
-            mbox:                     opts.mbox                   || settings.globalMboxName,
-            timeout:                  opts.timeout                || settings.timeout,
-            globalMboxAutoCreate:     settings.globalMboxAutoCreate,
-            params:                   opts.params                 || null,
-            selector:                 opts.selector               || null,
-            allowedRoutesFilter:      opts.allowedRoutesFilter    || [],
-            disallowedRoutesFilter:   opts.disallowedRoutesFilter || [],
-            appendToSelector:         opts.appendToSelector       || false,
-            debug:                    opts.debug                  || false
-          };
-        }])
-
-      .service('service', ['options', '$q', 'logger', ServiceFunction]);
+    };
   }
-});
+
+  function getOptions(settings, opts) {
+    return {
+      mbox: opts.mbox || settings.globalMboxName,
+      timeout: opts.timeout || settings.timeout,
+      globalMboxAutoCreate: settings.globalMboxAutoCreate,
+      params: opts.params || null,
+      selector: opts.selector || null,
+      allowedRoutesFilter: opts.allowedRoutesFilter || [],
+      disallowedRoutesFilter: opts.disallowedRoutesFilter || [],
+      appendToSelector: opts.appendToSelector || false
+    };
+  }
+
+  adobe.target.registerExtension({
+    name: 'angular.directive',
+    modules: ['settings', 'logger'],
+    register: function (settings, logger) {
+      angular.module('angular.directive.lib', [])
+        .constant('version', '0.3.0')
+        .constant('settings', settings)
+        .constant('logger', logger)
+        .constant('customOptions', {})
+
+        .factory('options', ['settings', 'customOptions', getOptions])
+
+        .service('service', ['options', '$q', 'logger', ServiceFunction]);
+    }
+  });
+})();
+

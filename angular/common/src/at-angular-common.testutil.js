4734570368c93d50e8eeffaf2be9397127c6ea22
Andrei Anischevici <anischev@adobe.com>
2016-07-19 20:25:20 +0300

    Add getOffer/applyOffer/settings mocks to testutil

--- angular/common/src/at-angular-common.testutil.js
+++ angular/common/src/at-angular-common.testutil.js
@@ -5,6 +5,15 @@ var adobe = {
     ext: {},
     registerExtension: function (params) {
       var exposedModules = {
+        settings: {
+          defaultContentHiddenStyle: 'visibility:hidden;',
+          defaultContentVisibleStyle: 'visibility:visible;',
+          bodyHiddenStyle: 'body{display:none}',
+          bodyHidingEnabled: true,
+          globalMboxAutoCreate: false,
+          timeout: 5000,
+          pollingAfterDomReadyTimeout: 2000
+        },
         logger: console
       };
 
@@ -14,6 +23,12 @@ var adobe = {
       });
 
       adobe.target.ext[params.name] = params.register.apply(null, args);
+    },
+    getOffer: function (opts) {
+      var offer = '<p>Sample Offer</p>';
+      opts.success(offer);
+    },
+    applyOffer: function (opts) {
     }
   }
 };

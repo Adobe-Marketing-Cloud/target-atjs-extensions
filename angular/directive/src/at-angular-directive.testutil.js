0e4e8368a2c13715d95df416e5ba9a0e7f2d94ad
Andrei Anischevici <anischev@adobe.com>
2016-07-21 16:04:53 +0300

    Add angular-directive testutils

--- angular/directive/src/at-angular-directive.testutil.js
+++ angular/directive/src/at-angular-directive.testutil.js
@@ -1,10 +1,29 @@
 'use strict';
 
+function buildNamespace(base, name, value) {
+  var parts = name.split('.');
+  for (var i = 0; i < parts.length - 1; i++) {
+    var part = parts[i];
+    base[part] = base[part] || {};
+    base = base[part];
+  }
+  base[parts[parts.length - 1]] = value;
+}
+
 var adobe = {
   target: {
     ext: {},
     registerExtension: function (params) {
       var exposedModules = {
+        settings: {
+          defaultContentHiddenStyle: 'visibility:hidden;',
+          defaultContentVisibleStyle: 'visibility:visible;',
+          bodyHiddenStyle: 'body{display:none}',
+          bodyHidingEnabled: true,
+          globalMboxAutoCreate: false,
+          timeout: 5000,
+          pollingAfterDomReadyTimeout: 2000
+        },
         logger: console
       };
 
@@ -13,7 +32,13 @@ var adobe = {
         args.push(exposedModules[elem]);
       });
 
-      adobe.target.ext[params.name] = params.register.apply(null, args);
+      buildNamespace(adobe.target.ext, params.name, params.register.apply(null, args));
+    },
+    getOffer: function (opts) {
+      var offer = '<p>Sample Offer</p>';
+      opts.success(offer);
+    },
+    applyOffer: function (opts) {
     }
   }
 };

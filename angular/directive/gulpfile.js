bc54501cff022adfc96139c7c663dc5787746f5f
Andrei Anischevici <anischev@adobe.com>
2016-07-25 20:27:56 +0300

    Change ChromeCanary to Chrome for test:debug gulp task

--- angular/directive/gulpfile.js
+++ angular/directive/gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('test:debug', done => {
   new Server({
     configFile: path.join(__dirname, '/karma.conf.js'),
     singleRun: false,
-    browsers: ['ChromeCanary'],
+    browsers: ['Chrome'],
     reporters: ['kjhtml']
   }, done).start();
 });

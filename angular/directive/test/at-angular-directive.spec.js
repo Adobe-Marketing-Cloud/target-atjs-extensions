2b6971bcff31c189915967e1cf6f278b8952e3bd
Andrei Anischevici <anischev@adobe.com>
2016-07-25 16:54:10 +0300

    Add append mbox directive test case Cleanup logging and build angular-directive

--- angular/directive/test/at-angular-directive.spec.js
+++ angular/directive/test/at-angular-directive.spec.js
@@ -3,8 +3,10 @@
 var app = angular.module('myApp', []);
 var $compile;
 var $rootScope;
+var element;
 var offerService;
 var options;
+var setOptionsAndLoadView;
 
 adobe.target.ext.angular.initDirective(app, {mbox: 'myMbox'});
 
@@ -22,10 +24,11 @@ describe('mbox directive tests', function () {
 
     spyOn(offerService, 'getOfferPromise').and.callThrough();
     spyOn(offerService, 'applyOfferPromise').and.callThrough();
+
+    element = $compile('<h1 mbox>Hello test!</h1>')($rootScope);
   }]));
 
   it('should call getOffer and applyOffer', function () {
-    var element = $compile('<h1 mbox>Hello test!</h1>')($rootScope);
     $rootScope.$digest();
     expect(offerService.getOfferPromise).toHaveBeenCalled();
     expect(offerService.applyOfferPromise).toHaveBeenCalled();
@@ -34,8 +37,6 @@ describe('mbox directive tests', function () {
   });
 
   it('should hide element before getOffer and reveal it after applyOffer', function () {
-    var element = $compile('<h1 mbox>Hello test!</h1>')($rootScope);
-
     expect(element.hasClass('mboxDefault')).toBe(true);
     $rootScope.$digest();
     expect(element.hasClass('mboxDefault')).toBe(false);
@@ -47,23 +48,42 @@ describe('Automatic element conversion to mbox directive', function () {
 
   beforeEach(inject(['$compile', '$rootScope', 'options',
     function (_$compile_, _$rootScope_, _options_) {
+      spyOn(adobe.target, 'getOffer').and.callThrough();
+      spyOn(adobe.target, 'applyOffer');
+
       $compile = _$compile_;
       $rootScope = _$rootScope_;
       options = _options_;
 
-      spyOn(adobe.target, 'getOffer').and.callThrough();
-      spyOn(adobe.target, 'applyOffer');
+      element = $compile('<h1>Hello test!</h1>')($rootScope);
+      var rootElement = angular.element(document.querySelectorAll('body'));
+      rootElement.append(element);
+
+      setOptionsAndLoadView = function (appendToSelector) {
+        options.selector = 'h1';
+        if (appendToSelector) {
+          options.appendToSelector = true;
+        }
+        $rootScope.$broadcast('$viewContentLoaded');
+      };
     }]));
 
   it('should replace matched selector with mbox directive', function () {
-    options.selector = 'h1';
-    var rootElement = angular.element(document.querySelectorAll('body'));
-    var element = $compile('<h1>Hello test!</h1>')($rootScope);
-
-    rootElement.append(element);
-    $rootScope.$broadcast('$viewContentLoaded');
+    setOptionsAndLoadView(false);
+    $rootScope.$digest();
     expect(element[0].attributes.mbox).toBeDefined();
     expect(element[0].attributes['data-mboxname'].value).toEqual('myMbox');
+    expect(adobe.target.getOffer).toHaveBeenCalled();
+    expect(adobe.target.applyOffer).toHaveBeenCalled();
+    element.remove();
+  });
+
+  it('should append mbox directive to matched selector', function () {
+    setOptionsAndLoadView(true);
+    element = element[0].lastChild;
+    expect(element.tagName).toEqual('DIV');
+    expect(element.attributes.mbox).toBeDefined();
+    expect(element.attributes['data-mboxname'].value).toEqual('myMbox');
     $rootScope.$digest();
     expect(adobe.target.getOffer).toHaveBeenCalled();
     expect(adobe.target.applyOffer).toHaveBeenCalled();

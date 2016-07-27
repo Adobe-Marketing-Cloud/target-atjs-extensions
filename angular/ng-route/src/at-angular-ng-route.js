a4c6812dd639093d82fdf18f8dc062f0a3ed7c34
Andrei Anischevici <anischev@adobe.com>
2016-07-27 17:28:51 +0300

    Implement routeService decorator, that adds applyTargetToRoutes functionality applyTargetToRoutes sets Target getOffer promise as allowed routes' resolve

--- angular/ng-route/src/at-angular-ng-route.js
+++ angular/ng-route/src/at-angular-ng-route.js
@@ -1,17 +1,52 @@
 
-/* global adobe */
-'use strict';
+/* global adobe, angular */
+(function (document, angular, at) {
+  'use strict';
 
-adobe.target.registerExtension({
-  name: 'myGreetingExtension',
-  modules: ['logger'],
-  register: function (logger) {
-    return function (name) {
-      var message = 'Hello, ' + name + '!';
-      logger.log(message);
-      return message;
-    };
+  function addModuleDependencies(module, dependencies) {
+    dependencies.forEach(function (dependency) {
+      if (module.requires.indexOf(dependency) === -1) {
+        module.requires.push(dependency);
+      }
+    });
   }
-});
 
-adobe.target.ext.myGreetingExtension('Geronimo');
+  function setTargetOfferResolve(route, offerPromise) {
+    route.resolve = route.resolve || {};
+    route.resolve.offerData = offerPromise;
+  }
+
+  function routeServiceDecorator($delegate, options, offerService, logger) {
+    $delegate.applyTargetToRoutes = function (locations) {
+      Object.keys(locations).forEach(function (routeName) {
+        logger.debug('location:' + routeName);
+        if ($delegate.isRouteAllowed(routeName, options.allowedRoutesFilter, options.disallowedRoutesFilter)) {
+          setTargetOfferResolve(locations[routeName], offerService.getOfferPromise);
+        }
+      });
+    };
+    return $delegate;
+  }
+
+  function decorateRouteService() {
+    angular.module('target-angular.common')
+      .decorator('routeService', ['$delegate', 'options', 'offerService', 'logger', routeServiceDecorator]);
+  }
+
+  function initializeModule(module) {
+  }
+
+  at.registerExtension({
+    name: 'angular.initRoutes',
+    modules: [],
+    register: function () {
+      return function (app, opts) {
+        at.ext.angular.setupCommon(opts);
+        decorateRouteService();
+        var appModule = (typeof app === 'string') ? angular.module(app) : app;
+        addModuleDependencies(appModule, ['target-angular.common']);
+        initializeModule(appModule);
+      };
+    }
+  });
+})(document, angular, adobe.target);

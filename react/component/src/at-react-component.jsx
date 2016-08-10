c15da8d0060d559878a436c614d3e0d340461818
Andrei Anischevici <anischev@adobe.com>
2016-08-10 18:17:30 +0300

    Implement at-react-component draft Add react and gulp-babel dev deps

---
+++ react/component/src/at-react-component.jsx
@@ -0,0 +1,47 @@
+
+/* global adobe, React */
+(function (React, at) {
+  'use strict';
+
+  at.registerExtension({
+    name: 'react.createMboxComponent',
+    modules: ['settings', 'logger'],
+    register: function () {
+      return function (opts) {
+        return React.createClass({
+          render: function () {
+            return <div ref={(ref) => {this.mboxDiv = ref}} {...this.props}>{this.props.children}</div>;
+          },
+
+          componentDidMount: function() {
+            logger.log('MboxComponentDidMount');
+
+            var component = this;
+            at.getOffer({
+              mbox: component.props.mbox,
+              params: component.props.params,
+              success: function (response) {
+                component.setState({
+                  offerData: response
+                });
+              },
+              error: function (status, error) {
+                logger.log('getOffer error: ', error, status);
+              }
+            });
+          },
+
+          componentDidUpdate: function() {
+            logger.log('MboxComponentDidUpdate');
+
+            adobe.target.applyOffer({
+              mbox: this.props.mbox,
+              offer: this.state.offerData,
+              element: this.mboxDiv
+            });
+          }
+        });
+      };
+    }
+  });
+})(React, adobe.target);

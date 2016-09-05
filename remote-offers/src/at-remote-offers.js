27e1577082cb7ca1f1d46fb95896a7f937d29c63
Andrei Anischevici <anischev@adobe.com>
2016-09-05 18:29:31 +0300

    Refactor extension init Remove dom dependency Remove options param Include window, document, adobe.target passing down Rename success/error callbacks Replace custom log with AT logger module

--- remote-offers/src/at-remote-offers.js
+++ remote-offers/src/at-remote-offers.js
@@ -1,5 +1,5 @@
 /*!
- * adobe.target.ext.remoteoffers.js v0.2.0
+ * adobe.target.ext.remoteoffers.js v0.3.0
  *
  * Copyright 1996-2016. Adobe Systems Incorporated. All rights reserved.
  *
@@ -9,124 +9,126 @@
         {
             'url': '/promotion1.html',          //remote offer url (required, must be same domain)
             'selector': '.menu a',              //CSS selector of element to deliver offer to
-            'callbackSuccess': function(){},    //successful callback
-            'callbackError':   function(){},    //error handler
+            'success': function(){},      //successful callback
+            'error':   function(){},      //error handler
             'method': 'replace'                 //method to handle offer injection into DOM: 'append' (default) or 'replace'
         },
         {
             'url': '/promotion2.html',
             'selector': '#banner'
         }
-    ],
-    {debug:true}
+    ]
     );
  */
-!(function(){
-    "use strict";
+(function(window, document, at){
+  'use strict';
 
-    var log = function(){};
+  var onReady = function(getobjs,callback,error, logger){
+    var interval = 400, timeout = 30000;
+    var checker = setInterval(function () {
+      var objs = getobjs();
+      if(timeout <= 0) {
+        clearInterval(checker);
+        if(typeof error==='function') error();
+        return;
+      }
+      var isMissing=false;
+      for(var i=0; i<objs.length; i++)
+        if(!objs[i])
+          isMissing=true;
+      if(isMissing===false){
+        clearInterval(checker);
+        logger.log('el is ready');
+        if(typeof callback==='function') callback();
+        return;
+      }
+      timeout -= interval;
+    }, interval);
+  };
 
-    var onReady = function(getobjs,callback,error){
-          var interval = 400, timeout = 30000;
-          var checker = setInterval(function () {
-            var objs = getobjs();
-            if(timeout <= 0) {
-              clearInterval(checker);
-              if(typeof error==='function') error();
-              return;
-            }
-            var isMissing=false;
-            for(var i=0; i<objs.length; i++)
-              if(!objs[i])
-                isMissing=true;
-            if(isMissing===false){
-              clearInterval(checker);
-              log('el is ready');
-              if(typeof callback==='function') callback();
-              return;
-            }
-            timeout -= interval;
-          }, interval);
-    };
+  var makeAjaxCall = function(url,callback,error) {
+      log('XHR to '+url);
+      var xmlhttp = new XMLHttpRequest();
+      xmlhttp.onreadystatechange = function() {
+          if (xmlhttp.readyState == 4 ) {
+              if(xmlhttp.status == 200)
+                  callback(xmlhttp.responseText);
+              else error(xmlhttp.status);
+          }
+      }
+      xmlhttp.open("GET", url, true);
+      xmlhttp.send();
+  };
+  //injecting CSS to hide containers
+  var addCssToHead = function(css) {
+      var head = document.getElementsByTagName("head")[0];
+      if (head) {
+          var style = document.createElement("style");
+          style.setAttribute("type", "text/css");
+          if (style.styleSheet) {
+              style.styleSheet.cssText = css;
+          } else {
+              style.appendChild(document.createTextNode(css));
+          }
+          head.insertBefore(style, head.firstChild);
+      }
+  };
 
-    var makeAjaxCall = function(url,callback,error) {
-        log('XHR to '+url);
-        var xmlhttp = new XMLHttpRequest();
-        xmlhttp.onreadystatechange = function() {
-            if (xmlhttp.readyState == 4 ) {
-                if(xmlhttp.status == 200)
-                    callback(xmlhttp.responseText);
-                else error(xmlhttp.status);
-            }
+  var getOffer = function(path, selector, success, error, method, logger) {
+      logger.log('getOffer');
+      var prehide = selector + '{visibility:hidden}';
+      addCssToHead(prehide);
+      makeAjaxCall(path, function(result){
+          logger.log('Call success, will applyOffer ' + path + ' to ' + selector);
+          onReady(function(){ return [document.querySelector(selector)] },
+                  function(){
+                      var element = document.querySelector(selector);
+                      if(typeof method === 'string' && method === 'replace'){
+                          var newNode = document.createElement('div');
+                          element.parentNode.replaceChild(newNode, element);
+                          element = newNode;
+                          logger.log('method:'+method);
+                      }
+                      at.applyOffer({
+                        'element': element,
+                        'offer': [{
+                          'type': 'html',
+                          'content': result
+                        }]
+                      });
+                      logger.log('Offer applied to '+selector);
+                      if(typeof success==='function'){ logger.log('Success handler'); success(); }
+                  },
+                  logger);
+      }, function(status){
+          logger.error("Error loading content for '" + path + "', status: '" + status);
+          var element = document.querySelector(selector);
+          if(element) element.style.visibility = 'visible';
+          if(typeof error==='function') { logger.error('Error handler'); error(); }
+      });
+
+  };
+
+  function fetchOffers(data, logger) {
+    data.filter(function (el) {
+        if (!el.url || !el.selector) {
+          logger.error('Missing URL or selector');
+          return false;
         }
-        xmlhttp.open("GET", url, true);
-        xmlhttp.send();
-    };
-    //injecting CSS to hide containers
-    var addCssToHead = function(css) {
-        var head = document.getElementsByTagName("head")[0];
-        if (head) {
-            var style = document.createElement("style");
-            style.setAttribute("type", "text/css");
-            if (style.styleSheet) {
-                style.styleSheet.cssText = css;
-            } else {
-                style.appendChild(document.createTextNode(css));
-            }
-            head.insertBefore(style, head.firstChild);
-        }
-    };
+        return true;
+      })
+      .forEach(function(el) {
+        getOffer(el.url, el.selector, el.success, el.error, el.method, logger);
+      });
+  }
 
-    var getOffer = function(path, selector, callbackSuccess, callbackError, method) {
-        log('getOffer');
-        var prehide = selector + '{visibility:hidden}';
-        addCssToHead(prehide);
-        makeAjaxCall(path, function(result){
-            log('Call success, will applyOffer '+path+' to '+selector);
-            onReady(function(){ return [document.querySelector(selector)] },
-                    function(){
-                        var element = document.querySelector(selector);
-                        if(typeof method === 'string' && method === 'replace'){
-                            var newNode = document.createElement('div');
-                            element.parentNode.replaceChild(newNode, element);
-                            element = newNode;
-                            log('method:'+method);
-                        }
-                        adobe.target.applyOffer({
-                          'element': element,
-                          'offer': [{
-                            'type': 'html',
-                            'content': result
-                          }]
-                        });
-                        log('Offer applied to '+selector);
-                        if(typeof callbackSuccess==='function'){ log('Success handler'); callbackSuccess(); }
-                    });
-        }, function(status){
-            log("Error loading content for '" + path + "', status: '" + status);
-            var element = document.querySelector(selector);
-            if(element) element.style.visibility = 'visible';
-            if(typeof callbackError==='function') { log('Error handler'); callbackError(); }
-        });
-
-    };
-
-    var _register = function(dom, logger) {
-      var getRemoteOffers = function(data, opts) {
-        log = (opts.debug) ? function(s){logger.log('ATX:'+s)} : log;
-        for (var i = 0; i < data.length; i++) {
-            var el = data[i];
-            if(el.url && el.selector)
-                getOffer(el.url, el.selector, el.callbackSuccess, el.callbackError, el.method);
-            else log('Error:missing URL or selector');
-        }
+  adobe.target.registerExtension({
+    name:'remoteoffers',
+    modules: ['logger'],
+    register: function (logger) {
+      return function (data) {
+        fetchOffers(data, logger);
       };
-      return getRemoteOffers;
-    };
-
-    adobe.target.registerExtension({
-        name:'remoteoffers',
-        modules: ['dom', 'logger'],
-        register: _register
-    });
-})();
+    }
+  });
+})(window, document, adobe.target);

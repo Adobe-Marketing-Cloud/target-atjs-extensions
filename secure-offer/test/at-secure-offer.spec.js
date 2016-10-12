f784411487f760bddc8b50074c71d62e669b48ac
Andrei Anischevici <anischev@adobe.com>
2016-10-12 12:17:23 +0300

    Add an HTML offer sanitization test

--- secure-offer/test/at-secure-offer.spec.js
+++ secure-offer/test/at-secure-offer.spec.js
@@ -1,6 +1,20 @@
-/* global adobe */
+/* global adobe testOfferContent */
+
 describe('greeter', function () {
-  it('should not fail', function () {
-    expect(true).toEqual(true);
+  var offer;
+  it('should sanitize HTML offers', function () {
+    testOfferContent = '<b style=\\"color: red;\\" onclick=\\"alert(1)\\">hello</b><img src=\\"http://asdf\\"><a href=\\"javascript:alert(0)\\"><script src=\\"http://dfd\\"><\/script>';
+    adobe.target.ext.getSecureOffer({
+      mbox: 'htmlTest',
+      success: function (response) {
+        offer = response;
+        console.log('Content: ', response);
+      }
+    }, {
+      urlTransformer: function (url) {
+        return url;
+      }
+    });
+    expect(offer.content).toEqual('<b style="color: red">hello</b><img src="http://asdf"><a></a>');
   });
 });
--- secure-offer/test/at-secure-offer.testutil.js
+++ secure-offer/test/at-secure-offer.testutil.js
@@ -1,10 +1,31 @@
 'use strict';
 
+function buildNamespace(base, name, value) {
+  var parts = name.split('.');
+  for (var i = 0; i < parts.length - 1; i++) {
+    var part = parts[i];
+    base[part] = base[part] || {};
+    base = base[part];
+  }
+  base[parts[parts.length - 1]] = value;
+}
+
+var testOfferContent = '<p>Sample Offer</p>';
+
 var adobe = {
   target: {
     ext: {},
     registerExtension: function (params) {
       var exposedModules = {
+        settings: {
+          defaultContentHiddenStyle: 'visibility:hidden;',
+          defaultContentVisibleStyle: 'visibility:visible;',
+          bodyHiddenStyle: 'body{display:none}',
+          bodyHidingEnabled: true,
+          globalMboxAutoCreate: false,
+          timeout: 5000,
+          pollingAfterDomReadyTimeout: 2000
+        },
         logger: console
       };
 
@@ -13,7 +34,26 @@ var adobe = {
         args.push(exposedModules[elem]);
       });
 
-      adobe.target.ext[params.name] = params.register.apply(null, args);
+      buildNamespace(adobe.target.ext, params.name, params.register.apply(null, args));
+    },
+    getOffer: function (opts) {
+      var offer;
+      switch (opts.mbox) {
+        case 'htmlTest' :
+          offer = '{\
+            "type": "html",\
+            "content": "' + testOfferContent + '"\
+          }';
+          break;
+        default:
+          offer = '{\
+            "type": "html",\
+            "content": "' + testOfferContent + '"\
+          }';
+      }
+      opts.success(JSON.parse(offer));
+    },
+    applyOffer: function (opts) {
     }
   }
 };

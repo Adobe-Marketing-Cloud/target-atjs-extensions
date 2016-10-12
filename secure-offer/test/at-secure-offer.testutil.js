5f918c15894092df6b1261490693a53eb82c0dbd
XDex <andrei002@gmail.com>
2016-10-12 18:33:35 +0300

    TNT-25905 Implement Secure Offer extension (#9)
    * Scaffold at-secure-offer extension
    
    * Add Google Caja JsHtmlSanitizer code
    Cleanup redundant licenses and window bindings
    Wrap extension code in an IIFE
    
    * Implement sanitizing for HTML offers
    
    * Remove offer plugins, if present
    Remove sample tests
    
    * Add an HTML offer sanitization test
    
    * Hide custom JsHtmlSanitizer options
    Set a default allowAll URL transformer
    
    * Implement Action offer sanitization
    Add corresponding test case
    
    * Update README
    
    * Add Yarn lockfile
    
    * Add multiple offers support
    
    * Add multiple offers sanitization test

---
+++ secure-offer/test/at-secure-offer.testutil.js
@@ -0,0 +1,45 @@
+'use strict';
+
+function buildNamespace(base, name, value) {
+  var parts = name.split('.');
+  for (var i = 0; i < parts.length - 1; i++) {
+    var part = parts[i];
+    base[part] = base[part] || {};
+    base = base[part];
+  }
+  base[parts[parts.length - 1]] = value;
+}
+
+var testOffer;
+
+var adobe = {
+  target: {
+    ext: {},
+    registerExtension: function (params) {
+      var exposedModules = {
+        settings: {
+          defaultContentHiddenStyle: 'visibility:hidden;',
+          defaultContentVisibleStyle: 'visibility:visible;',
+          bodyHiddenStyle: 'body{display:none}',
+          bodyHidingEnabled: true,
+          globalMboxAutoCreate: false,
+          timeout: 5000,
+          pollingAfterDomReadyTimeout: 2000
+        },
+        logger: console
+      };
+
+      var args = [];
+      params.modules.forEach(function (elem) {
+        args.push(exposedModules[elem]);
+      });
+
+      buildNamespace(adobe.target.ext, params.name, params.register.apply(null, args));
+    },
+    getOffer: function (opts) {
+      opts.success(JSON.parse(testOffer));
+    },
+    applyOffer: function (opts) {
+    }
+  }
+};

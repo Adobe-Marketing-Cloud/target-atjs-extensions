e21d586c9a36c44574a76e788e75a64f2ea082ad
Andrei Anischevici <anischev@adobe.com>
2017-12-27 18:23:54 +0200

    Update Github Pages with latest demos

--- examples/angular/js/state-change-demo-app.js
+++ examples/angular/js/state-change-demo-app.js
@@ -1,22 +1,17 @@
 var sampleApp = angular.module('sampleApp', ['ui.router']);
 
-sampleApp.config(function($stateProvider, $urlRouterProvider) {
+sampleApp.config(function($stateProvider, $urlServiceProvider) {
     
-    $urlRouterProvider.otherwise('/view1');
+    $urlServiceProvider.rules.otherwise('/view1');
     
     $stateProvider
-        
-        // HOME STATES AND NESTED VIEWS ========================================
         .state('view1', {
             url: '/view1',
             templateUrl: 'partials/view1.html'
         })
-        
-        // ABOUT PAGE AND MULTIPLE NAMED VIEWS =================================
         .state('view2', {
             url: '/view2',
-            templateUrl: 'partials/view2.html',
-            resolve: {}   
+            templateUrl: 'partials/view2.html'
         });
-        
+
 });
